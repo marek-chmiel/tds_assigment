c27f8759a9c7704141ab9b2a5dda8f77595feabb
Marek Chmiel <marek.chmiel@op.pl>
2026-08-14 18:23:11 +0200

    Story: Java assignment
    Init commit

---
+++ ParkingLot/src/test/java/uk/co/testdrivensolutions/parkinglot/ParkingLotApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.testdrivensolutions.parkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
